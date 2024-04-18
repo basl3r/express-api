fc81512f6fc1f623ae4930c34b3918f3223a8371
Adrian Grociak <ajgrociak@gmail.com>
2024-04-18 13:19:57 +0200

    fix issues

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const testimonialsRoutes = require('./testimonials.routes');
 const concertsRoutes = require('./concerts.routes');
@@ -8,15 +9,20 @@ const seatsRoutes = require('./seats.routes');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use('/api/testimonials', testimonialsRoutes);
 app.use('/api/concerts', concertsRoutes);
 app.use('/api/seats', seatsRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).send('404 not found...');
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
